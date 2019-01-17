d8847b313eba45aca8aed62eca28ee9195b77103
pydiprasanna <45852510+pydiprasanna@users.noreply.github.com>
2019-01-17 12:30:04 +0530

    Add files via upload

---
+++ DeveloperTestExercise-master/FileData/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using ThirdPartyTools;
+
+namespace FileData
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            FileDetails objFileDetails = new FileDetails();
+            try
+            {
+                if (args != null)
+                {
+                    string[] version = new string[] { "–v", "--v", "/v", "--version" };
+                    string[] size = new string[] { "–s", "--s", "/s", "--size" };
+                    if (version.Contains(args[0]))
+                        Console.WriteLine("Version of the file is :" + objFileDetails.Version(args[1]));
+                    if (size.Contains(args[0]))
+                        Console.WriteLine("Size of the file is: " + objFileDetails.Size(args[1]));
+
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                objFileDetails = null;
+            }
+        }
+    }
+}
